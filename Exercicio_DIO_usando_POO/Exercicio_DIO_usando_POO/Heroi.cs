c29cb57744c2da578429530653c58dd039b1cb41
madson <madsonamora@yahoo.com.br>
2022-02-02 18:17:17 -0300

    primeiro commit

---
+++ Exercicio_DIO_usando_POO/Exercicio_DIO_usando_POO/Heroi.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_DIO_usando_POO
+{
+    public class Arus
+    {
+        public string name { get; set; }
+        public int level { get; set; } = 0;
+        public string heroType { get; set; }
+
+
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.name = Name;
+            this.level = Level;
+            this.heroType = HeroType;
+
+        }
+
+        public override string ToString() { return name; }
+        public string atack() { return name +" atacou"; }
+    }
+}
---
+++ Exercicio_DIO_usando_POO/Exercicio_DIO_usando_POO/Program.cs
@@ -0,0 +1,13 @@
+﻿using Exercicio_DIO_usando_POO;
+
+
+
+
+Arus hero = new Arus("jose", 1, "wizard");
+Arus oponent = new Arus("carlos", 2, "knight");
+
+Console.WriteLine(hero.ToString());
+Console.WriteLine(oponent.ToString());
+Console.WriteLine(hero.atack());
+
+Console.ReadKey(); 
